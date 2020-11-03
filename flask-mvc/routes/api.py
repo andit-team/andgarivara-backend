aa9a11083f3622a1808e8c0051776d2e54dea76e
Tama Sarker <tamasarker112@gmail.com>
2020-11-03 14:47:44 +0600

    Admin Dashboard with update api

--- flask-mvc/routes/api.py
+++ flask-mvc/routes/api.py
@@ -7,12 +7,10 @@ from controllers.users.register.signup import UserSignup
 from controllers.users.vehicles.vehicleOp import AddVehicle, GetVehicleTypeDataField, EditVehicle, DeleteVehicle, UserVehicleList
 
 ########################## Admin ##########################
-from controllers.admin.fuel_type.fuelTypeOperation import FuelTypeList, AddFuelType, EditFuelType
-from controllers.admin.location.adminAreaOp import AreaList, AddArea, EditArea, DeleteArea
-from controllers.admin.location.adminCityOp import CityList, AddCity, EditCity, DeleteCity
+from controllers.admin.fuel_type.fuelTypeOperation import FuelTypeList, AddFuelType, EditFuelType, FuelTypeById
 from controllers.admin.register.login import AdminLogin
 from controllers.admin.register.signup import AdminSignup
-from controllers.admin.vehicles.adminVehicleTypeOp import AddVehicleType, EditVehicleType, DeleteVehicleType, VehicleTypeList,AddBrandWithVehicleType, EditBrandWithVehicleType, VehicleBrandList
+from controllers.admin.vehicles.adminVehicleTypeOp import AddVehicleType, EditVehicleType, VehicleTypeById, DeleteVehicleType, VehicleTypeList,AddBrandWithVehicleType, EditBrandWithVehicleType, BrandWithVehicleTypeById, VehicleBrandList
 from controllers.admin.users.adminUser import UserList, DeleteUser, AddUser
 from controllers.admin.vehicles.adminVehicleOp import AdminVehicleList, AddVehicleAdmin, EditVehicleAdmin, DeleteVehicleAdmin
 ########################## Frontend ##########################
@@ -37,7 +35,8 @@ def create_routes(api: Api):
     ########################## Admin.FuelType ##########################
     api.add_resource(FuelTypeList, '/api/fuel_type_list')
     api.add_resource(AddFuelType, '/api/admin/add_fuel_type')
-    api.add_resource(EditFuelType, '/api/admin/edit_fuel_type')
+    api.add_resource(FuelTypeById, '/api/admin/edit_fuel_type')
+    api.add_resource(EditFuelType, '/api/admin/update_fuel_type')
     
     ########################## Admin.register ##########################
     api.add_resource(AdminLogin, '/api/admin/login')
@@ -46,10 +45,14 @@ def create_routes(api: Api):
     api.add_resource(VehicleTypeList, '/api/vehicle_type_list')
     api.add_resource(AddVehicleType, '/api/admin/add_vehicle_type')
     api.add_resource(EditVehicleType, '/api/admin/edit_vehicle_type')
+    api.add_resource(VehicleTypeById, '/api/admin/update_vehicle_type')
     api.add_resource(DeleteVehicleType, '/api/admin/delete_vehicle_type')
+    ########################## Admin.vehicle.vehicleBrand ##########################
     api.add_resource(AddBrandWithVehicleType, '/api/admin/add_vehicle_brand')
     api.add_resource(EditBrandWithVehicleType, '/api/admin/edit_vehicle_brand')
+    api.add_resource(BrandWithVehicleTypeById, '/api/admin/update_vehicle_brand')
     api.add_resource(VehicleBrandList, '/api/vehicle_brand_list')
+     ########################## Admin.vehicle.User ##########################
     api.add_resource(UserList, '/api/admin/user_list')
     # api.add_resource(DeleteUser, '/admin/delete_user')
     # api.add_resource(AddUser, '/admin/add_user')
