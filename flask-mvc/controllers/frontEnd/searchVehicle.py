70a89228872a6ce80445ec5d951e74d851474311
Tama Sarker <tamasarker112@gmail.com>
2020-11-23 17:00:11 +0600

    Homepage & search vehicle

--- flask-mvc/controllers/frontEnd/searchVehicle.py
+++ flask-mvc/controllers/frontEnd/searchVehicle.py
@@ -1,3 +1,4 @@
+from os import error
 from flask import Response, request, jsonify
 from flask_restful import Resource
 from extension import mongo
@@ -5,251 +6,46 @@ import bson.json_util as bsonO
 import datetime
 import json
 from bson.json_util import dumps
-
+import constants.constantValue as constants
 
 class SearchVehicle(Resource):
     @staticmethod
     def post() -> Response:
+        msg = None
+        error = None
+        dtVehicle = []
         data = request.get_json()
-        if data["vehicle_type"] and not data["area"]:
-            vId = bsonO.ObjectId(data["vehicle_type"])
-            dt = SearchVehicleByType(vId)
-        elif not data["vehicle_type"] and data["area"]:
-            area = bsonO.ObjectId(data["area"])
-            dt = SearchVehicleByLocation(area)
-        elif not data["vehicle_type"] and not data["area"]:
-            dt = SearchVehicleAll()
-        else:
-            vId = bsonO.ObjectId(data["vehicle_type"])
-            area = bsonO.ObjectId(data["area"])
-            dt = SearchVehicleFIlter(vId, area)
-        return dt
-
-
-def SearchVehicleByLocation(area):
-    try:
-        err_msg = None
-        dt = mongo.db.vehicles.aggregate(
-            [{
-                "$match": {
-                    "area": area,
-                    "del_status": False
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "vehicle_types",
-                    "localField": "vehicle_type",
-                    "foreignField": "_id",
-                    "as": "vehicle_type_details"
-                },
-
-            },
-                {
-                "$lookup": {
-                    "from": "users",
-                    "localField": "user_id",
-                    "foreignField": "_id",
-                    "as": "user_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationCity",
-                    "localField": "city",
-                    "foreignField": "_id",
-                    "as": "city_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationArea",
-                    "localField": "area",
-                    "foreignField": "_id",
-                    "as": "area_details"
-                }
-            }
-            ])
-        msg = "SUCCESS"
-        error = False
-    except Exception as ex:
-        msg = "FAILED"
-        error = True
-        err_msg = ex
-    return jsonify({
-        "msg": msg,
-        "error": error,
-        "data": json.loads(dumps(dt))
-    })
-
-
-def SearchVehicleByType(vId):
-    try:
-        err_msg = None
-        dt = mongo.db.vehicles.aggregate(
-            [{
-                "$match": {
-                    "vehicle_type": vId,
-                    "del_status": False
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "vehicle_types",
-                    "localField": "vehicle_type",
-                    "foreignField": "_id",
-                    "as": "vehicle_type_details"
-                },
-
-            },
-                {
-                "$lookup": {
-                    "from": "users",
-                    "localField": "user_id",
-                    "foreignField": "_id",
-                    "as": "user_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationCity",
-                    "localField": "city",
-                    "foreignField": "_id",
-                    "as": "city_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationArea",
-                    "localField": "area",
-                    "foreignField": "_id",
-                    "as": "area_details"
-                }
-            }
-            ])
-        msg = "SUCCESS"
-        error = False
-    except Exception as ex:
-        msg = "FAILED"
-        error = True
-        err_msg = ex
-    return jsonify({
-        "msg": msg,
-        "error": error,
-        "data": json.loads(dumps(dt))
-    })
-
-
-def SearchVehicleFIlter(vId, area):
-    try:
-        err_msg = None
-        dt = mongo.db.vehicles.aggregate(
-            [{
-                "$match": {
-                    "vehicle_type": vId,
-                    "area": area,
-                    "del_status": False
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "vehicle_types",
-                    "localField": "vehicle_type",
-                    "foreignField": "_id",
-                    "as": "vehicle_type_details"
-                },
-
-            },
-                {
-                "$lookup": {
-                    "from": "users",
-                    "localField": "user_id",
-                    "foreignField": "_id",
-                    "as": "user_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationCity",
-                    "localField": "city",
-                    "foreignField": "_id",
-                    "as": "city_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationArea",
-                    "localField": "area",
-                    "foreignField": "_id",
-                    "as": "area_details"
-                }
-            }
-            ])
-
-        msg = "SUCCESS"
-        error = False
-    except Exception as ex:
-        msg = "FAILED"
-        error = True
-        err_msg = ex
-    return jsonify({
-        "msg": msg,
-        "error": error,
-        "data": json.loads(dumps(dt))
-    })
-
-
-def SearchVehicleAll():
-    try:
-        err_msg = None
-        dt = mongo.db.vehicles.aggregate(
-            [{
-                "$match": {
-                    "del_status": False
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "vehicle_types",
-                    "localField": "vehicle_type",
-                    "foreignField": "_id",
-                    "as": "vehicle_type_details"
-                },
-
-            },
-                {
-                "$lookup": {
-                    "from": "users",
-                    "localField": "user_id",
-                    "foreignField": "_id",
-                    "as": "user_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationCity",
-                    "localField": "city",
-                    "foreignField": "_id",
-                    "as": "city_details"
-                }
-            },
-                {
-                "$lookup": {
-                    "from": "locationArea",
-                    "localField": "area",
-                    "foreignField": "_id",
-                    "as": "area_details"
-                }
-            }
-            ])
-        msg = "SUCCESS"
-        error = False
-    except Exception as ex:
-        msg = "FAILED"
-        error = True
-        err_msg = ex
-    return jsonify({
-        "msg": msg,
-        "error": error,
-        "data": json.loads(dumps(dt))
-    })
+        queryCodition = {
+            "activeService": data["service"], "activeStatus" : constants.STATUS_VERIFIED, "del_status" : False,
+                                                      "carLocation": {
+                                                                    "$near": {
+                                                                    "$maxDistance": 20000,
+                                                                    "$geometry": {
+                                                                    "type": "Point",
+                                                                    "coordinates": [data["long"],data["lat"]]
+                                                                    }
+                                                                }
+                                                            }
+        }
+        if data["vehicleType"] :
+            queryCodition["vehicleType"] = bsonO.ObjectId(data["vehicleType"])
+        
+        dtVehicle = mongo.db.vehicles.find(queryCodition,
+                                                     {
+                                                         "_id" : 1,
+                                                         "vehicleTitle" : 1,
+                                                         "carLocation" : 1,
+                                                         "carAddress" : 1,
+                                                         "vehicle_imgs" : 1,
+                                                         "model" : 1,
+                                                         "manufactureYear" : 1,
+                                                         "ac" : 1,
+                                                         "millage" : 1,
+                                                         "serviceDetails.perDayRent" : 1,
+                                                     }
+                                                     )
+        return jsonify({
+            "msg": msg,
+            "error": error,            
+            "data": json.loads(dumps(dtVehicle))
+        })
