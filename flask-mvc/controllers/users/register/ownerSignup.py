b378db482128ff8f998964e9c04e61802765aee7
Tama Sarker <33072138+tamasarker1434@users.noreply.github.com>
2020-12-19 11:48:14 +0600

    owner Reg

---
+++ flask-mvc/controllers/users/register/ownerSignup.py
@@ -0,0 +1,52 @@
+from flask import Response, request, jsonify
+from flask_restful import Resource
+from extension import mongo
+import bson.json_util as bsonO
+import datetime
+import json
+from bson.json_util import dumps
+from werkzeug.security import generate_password_hash
+import constants.constantValue as constants
+
+
+class UserSignup(Resource):
+    @staticmethod
+    def post() -> Response:
+        data = request.get_json()
+        dt = {
+            "phone_no": data["phone_no"],
+            "password": generate_password_hash(data["password"]),
+            "first_name": data["first_name"],
+            "last_name": data["last_name"],
+            "email": data["email"],
+            "role": constants.ROLL_OWNER,
+            "address": data["address"],
+            "country": data["country"],
+            "pushNotification": {
+                "on_message_send": True,
+                "on_booking": True,
+                "on_suppport_reply": True
+            },
+            "smsNotification": {
+                "on_message_send": True,
+                "on_booking": True,
+                "on_suppport_reply": True
+            },
+            "default_contact_number" :data["phone_no"],
+            "profile_pic" :data["profile_pic"],
+            "del_status":False,
+            "create_date": datetime.datetime.now()
+        }
+        try:
+            indexCreate = mongo.db.userRegister.create_index(
+                'phone_no', unique=True)
+            ins = mongo.db.userRegister.insert_one(dt)
+            msg = "Inserted Successfully"
+            error = False
+        except Exception as ex:
+            msg = str(ex)
+            error = True
+        return jsonify({
+            "msg": msg,
+            "error": error
+        })
